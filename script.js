96dbe39487b1edf5b2acb8ac89a4f863b5c4526a
peep <sokolenok220@gmail.com>
2023-12-12 12:29:33 +0300

    finishchange

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 "use strict"
 
 
-let fam=prompt("Какая у вас фамилия?");
-let nam=prompt("Как вас зовут?");
-let otch=prompt("Какое у вас отчество?");
-let age=prompt("Сколько вам лет?");
-let gen=confirm("Ваш пол мужской?");
-let age2=parseFloat(age);
+let fam
+let nam
+let otch
+let age
 let pens="";
+
+do {
+    fam = prompt("Какая у вас фамилия?")
+} while (!fam)
+
+do {
+    nam = prompt("Какое у вас имя?")
+} while (!nam)
+
+do {
+    otch = prompt("Какое у вас отчество?");
+} while (!otch)
+
+do {
+    age=prompt("Сколько вам лет?");
+} while (!age)
+
+let gen=confirm("Ваш пол мужской?");
 if (gen===true) {
     gen="Мужской";
 } else {
     gen="Женский";
 }
 
-// if (age2>55 && gen===false) {
-//     pens="Да";
-// } else {
-//     pens="Нет";
-// }
+let age2=parseFloat(age);
 
-// if (age2>60 && gen===true) {
-//     pens="Да";
-// } else {
-//     pens="Нет";
-// }
+if (age2>55) {
+    pens="Да";
+} else {
+    pens="Нет";
+}
 
-
-alert( ("Ваше ФИО:"+(' ' + fam + ' ' + nam + ' ' + otch)) + '\n' + ("Ваш возраст в годах:" + ' ' + age2) + '\n' + ("Ваш возраст в днях:" + ' ' + age2*12*30) + '\n' + ("Через 5 лет вам будет:" + ' ' + (age2+5)) + '\n' + ("Ваш пол:") + ' ' + gen );
+alert( ("Ваше ФИО:"+(' ' + fam + ' ' + nam + ' ' + otch)) + '\n' + ("Ваш возраст в годах:" + ' ' + age2) + '\n' + ("Ваш возраст в днях:" + ' ' + age2*12*30) + '\n' + ("Через 5 лет вам будет:" + ' ' + (age2+5)) + '\n' + ("Ваш пол:") + ' ' + gen + '\n' + ("Вы на пенсии:") + ' ' + pens );
